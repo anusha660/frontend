c1f829d76f3d4ad5debffec407eef9b1b44109f6
anusha660 <anushakkp123@gmail.com>
2025-03-15 12:54:27 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
+export default function App() {
+  const [username, setUsername] = useState("");
+
+  async function handleClick() {
+
+      const response = await axios.post("https://apsbackend-v6jr.onrender.com/login", { username });
+      console.log(response.data);
+  
+  }
+
+  return (
+    <>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <button onClick={handleClick}>Submit</button>
+    </>
+  );
+}
